2aeca60515287c4d6b3c2a1f54a3a7f81f83d52a
Allen Ziegenfus <allen.ziegenfus@liferay.com>
2021-06-13 08:44:57 -0500

    Convert metadata files to text

--- convert_pandoc.sh
+++ convert_pandoc.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
 BASE=`basename $1 .docx`
 DIR=`dirname $1`
-pandoc -t markdown $1 -o $DIR/$BASE.md
+
+METADATA_FILENAMES=(beschreibung name geburtsdaten)
+
+echo "Processing $1"
+
+for METADATA_FILENAME in ${METADATA_FILENAMES[@]}; do
+	if [ "$BASE" = "$METADATA_FILENAME" ]
+	then
+		pandoc -t plain $1 -o $DIR/$BASE.txt
+	else
+		pandoc -t markdown $1 -o $DIR/$BASE.md
+	fi
+done
